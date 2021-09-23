151a2d8d94f54d130c52dac77b43c4d3d81240f1
goergk <grzegorzkaw124@gmail.com>
2021-09-23 19:50:58 +0200

    Post model added

--- project4/network/models.py
+++ project4/network/models.py
@@ -4,3 +4,13 @@ from django.db import models
 
 class User(AbstractUser):
     pass
+
+class Post(models.Model):
+    creator =  models.ForeignKey(User, on_delete=models.CASCADE, related_name="post_creator")
+    title = models.CharField(max_length=64)
+    content = models.CharField(max_length=2500)
+    creation_date = models.DateTimeField(auto_now_add=True)
+    edit_date = models.DateTimeField(null=True, blank=True)
+    likes = models.ManyToManyField(User, related_name="liked_posts", blank=True)
+
+
